3760299d8cd6b7c61629b0607947c670e1f0f203
rikarani <46773591+rikarani@users.noreply.github.com>
2026-07-20 14:20:34 +0700

    feat: initialize database configuration file with support for multiple drivers

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
 
+use Pdo\Mysql;
 use Illuminate\Support\Str;
 
 return [
@@ -59,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
@@ -79,7 +80,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
